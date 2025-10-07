2fb096286476cd2dc9a53879332fd2c71c16a2ed
Ivan Rolla <rollaivanh@hotmail.com>
2025-10-07 15:33:51 -0300

    BackendConection

--- frontend/src/app/products/new/product-form.tsx
+++ frontend/src/app/products/new/product-form.tsx
@@ -4,10 +4,16 @@ import { Label } from "@radix-ui/react-label";
 import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
 import { Input } from "@/components/ui/input";
+import { createProduct } from "../products.api";
 
 export function ProductForm() {
   const { register, handleSubmit } = useForm();
-  const onSubmit = handleSubmit((data) => {});
+  const onSubmit = handleSubmit(async (data) => {
+    console.log(data)
+    await createProduct({...data,
+      price: parseFloat(data.price)
+    })
+  });
 
   return (
     <form className="space-y-4" onSubmit={onSubmit}>
@@ -18,17 +24,17 @@ export function ProductForm() {
 
       <div className="space-y-2">
         <Label>Description</Label>
-        <Input {...register("Description")} />
+        <Input {...register("description")} />
       </div>
 
       <div className="space-y-2">
         <Label>Price</Label>
-        <Input {...register("Price")} />
+        <Input {...register("price")} />
       </div>
 
       <div className="space-y-2">
         <Label>Image</Label>
-        <Input {...register("imagen")} />
+        <Input {...register("image")} />
       </div>
 
       <Button className="w-full">Create Product</Button>
